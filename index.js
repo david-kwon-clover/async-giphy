cd1f9efdc0d4d05763a46999dd68142e6734b984
David Kwon <david.kwon@clover.com>
2024-11-04 14:04:03 -0700

    Add error handling in fetch

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const searchInput = document.querySelector("#search-input");
 const searchButton = document.querySelector("#search-button");
 const gifSection = document.querySelector(".gifSection");
+const errorMessage = document.querySelector(".errorMessage")
 
 searchButton.addEventListener("click", () => {
   gifSection.innerHTML = "";
   const urls = fetch(`http://localhost:3000/${searchInput.value}`)
-    .then((response) => response.json())
+    .then((response) => {
+      if(!response.ok) {
+        throw new Error("Sorry failed to fetch data..");
+      }
+      return response.json();
+    })
     .then((data) =>
       data.forEach((element) => {
         const gifCard = document.createElement("div");
@@ -13,5 +19,5 @@ searchButton.addEventListener("click", () => {
         gifSection.appendChild(gifCard);
       })
     )
-    .catch((error) => console.log(error));
+    .catch((error) => errorMessage.innerHTML = error.message);
 });
